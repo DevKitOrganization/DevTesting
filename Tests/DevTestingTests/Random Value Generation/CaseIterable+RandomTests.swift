eaaeed724c0824e2886cfb31553643bb5c55cdbb
Prachi Gauriar <prachi@gauriar.com>
2025-08-28 21:59:12 -0400

    Enable MemberImportVisibility upcoming feature across all targets

--- Tests/DevTestingTests/Random Value Generation/CaseIterable+RandomTests.swift
+++ Tests/DevTestingTests/Random Value Generation/CaseIterable+RandomTests.swift
@@ -7,6 +7,7 @@
 
 import DevTesting
 import Foundation
+import RealModule
 import Testing
 
 struct CaseIterable_RandomTests {
--- Tests/DevTestingTests/Random Value Generation/URLQueryItem+RandomTests.swift
+++ Tests/DevTestingTests/Random Value Generation/URLQueryItem+RandomTests.swift
@@ -7,6 +7,7 @@
 
 import DevTesting
 import Foundation
+import RealModule
 import Testing
 
 struct URLQueryItem_RandomTests {
